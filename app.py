a6bd0300f475afbb22c535245b410fb97e5dc1a3
richcastro82 <rich.castro@achesongroup.com>
2021-10-30 02:14:33 -0400

    Create app.py
    Main application file

---
+++ app.py
@@ -0,0 +1,11 @@
+#Richard Castro
+#October 2021
+#Holiday Topbar
+
+
+#Import LIBRARIES
+from datetime import date
+from FUNCTIONS import *
+
+if __name__ == '__main__':
+    app.run_server(host="127.0.0.1", port="8050", debug=True)
