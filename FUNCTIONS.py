6b53a6bb34b0d2f3bfcf2e5629d110d0719b8663
richcastro82 <rich.castro@achesongroup.com>
2021-11-05 10:07:47 -0400

    Updates
    formatting on the database dates, commenting on functions, update the date structure on function.

--- FUNCTIONS.py
+++ FUNCTIONS.py
@@ -2,14 +2,20 @@
 #Functions for holiday Topbar
 from LIBRARIES import *
 from datetime import datetime
-date = datetime.now()
-dat=date.strftime("%m/%d")
-displayHoliday='No Holiday'
+date = datetime.now().strftime("%m/%d")
+
+
+displayHoliday='no holiday'
+#Create a python list instead of the single variable above to hold multiple holiday on the same day.
+
+
 
 holidays=pd.read_csv('Holiday-List.csv', header=None, index_col=0, squeeze=True).to_dict()
 for key,value in holidays.items():
-    if (key == dat):
+    if (key == date):
         displayHoliday=value
+#Change this to append the list of holidays on the same day above instead of just changing the single variable.
+
 
 app = dash.Dash(
         __name__,
@@ -19,15 +25,25 @@ app = dash.Dash(
             "content": "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no",
             }],)
 
+
 server = app.server
 app.config["suppress_callback_exceptions"] = True
+
 
 app.layout = html.Div(
     className="box",
     children=[
-            html.Div(
-            id='', className="contentBox",children=[
+        html.Div(
+        id='',
+        className="contentBox",
+        children=[
             html.A(
-            id='', className="content",children=[dat,' - ',displayHoliday])
-            ])
-            ])
+            id='',
+            className="content",
+            children=[
+                date,' - ',displayHoliday
+                ]
+            )
+        ])
+    ]
+)
